ce9ac6dc4ba0e10862bd3909645d9b2a35b23010
mdrobnjak03 <83250516+mdrobnjak03@users.noreply.github.com>
2021-04-27 00:02:19 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'ucenik');
+  define('DB_PASS', '1234');
+  define('DB_NAME', 'ucenik');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://37.0.71.52/s3b/drobnjak.matija/shareposts');
+  // Site Name
+  define('SITENAME', 'SharePosts');
+  // App Version
+  define('APPVERSION', '1.0.0');
